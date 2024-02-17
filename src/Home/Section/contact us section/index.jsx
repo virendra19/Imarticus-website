79d9a0d9bbd0792b3275ba84f552ddf67bdc40b6
teerthpatel18 <54196748+ttpatel@users.noreply.github.com>
2024-02-17 09:31:41 +0530

    dropdown added

--- src/Home/Section/contact us section/index.jsx
+++ src/Home/Section/contact us section/index.jsx
@@ -2,24 +2,36 @@ import React from "react";
 import './contact.css'
 
 export default function ContactUs() {
+  const contactInfo = [
+    {
+      icon: "https://cdn.pegasus.imarticus.org/location/location.svg",
+      title: "Location",
+      address: "13th Floor, Gala Empire, Opp. T.V. Tower, Drive-In Rd, Memnagar, Ahmedabad, Gujarat, 380054"
+    },
+    {
+      icon: "	https://cdn.pegasus.imarticus.org/location/email-marketing.svg",
+      title: "Email",
+      email: "Info@imarticus.com"
+    },
+    {
+      icon: "	https://cdn.pegasus.imarticus.org/location/phone.svg",
+      title: "Call Us",
+      number: "+91 9953431020"
+    }
+  ];
+
   return (
     <div className="same_width_container">
       <div className="three_container">
-        {[1, 1, 1]?.map((item, index) => {
-          return (
-            <div key={index} className="logo_title_subtitle_container">
-              <img
-                className="logo_self"
-                src="https://cdn.pegasus.imarticus.org/location/location.svg"
-              />
-              <p>Location</p>
-              <p>
-                13th Floor, Gala Empire, Opp. T.V. Tower, Drive-In Rd, Memnagar,
-                Ahmedabad, Gujarat, 380054 
-              </p>
-            </div>
-          );
-        })}
+        {contactInfo.map((item, index) => (
+          <div key={index} className="logo_title_subtitle_container">
+            <img className="logo_self" src={item.icon} alt={item.title} />
+            <p>{item.title}</p>
+            {item.address && <p>{item.address}</p>}
+            {item.email && <p>{item.email}</p>}
+            {item.number && <p>{item.number}</p>}
+          </div>
+        ))}
       </div>
     </div>
   );
